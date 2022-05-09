cf933925187e5ca021b993aa21d151d038de3589
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-09 17:27:43 +0200

    push

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 function Random(randomnum){return (Math.floor(Math.random() * 100));}
 let Box1 = [];
 let Box2 = [];
-function Num (insnum) {return Number(prompt("Inserisci un numero"));}
+function Num (insnum) {return (Number(prompt("Inserisci un numero")));}
 if( Box2.length <= 15){
-     Num;
-    for(let i = 0; i <= 15 ; i++){
-        Box1.push(Num());
-        }
+     Num();
     }
 
     for(let i = 0; i <= 15 ; i++){
