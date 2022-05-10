98f981d74a711e47a39a55de9f8c33d6524368ad
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-10 15:27:17 +0200

    finito

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
-function Random(){return Math.floor(Math.random() * 100);}
-let Box1 = [];
-let Box2 = [];
-function Num (){return Number(prompt("Inserisci un numero"));}
-if( Box2.length <= 15){
-     Num();
+function Random(min, max) {
+    return Math.floor(Math.random() * (max - min+ 1) )+ min;
+  }
+    Game=Number(prompt("Scegli livello di difficolta 0=facile 1=medio 2=difficile"));
+    let difficulty;
+    if(Game=== 0){
+        difficulty = 10;
+    }else if(Game===1){
+        difficulty = 50;
+    }else{
+        difficulty = 100;
+    }
+    const bombs = [];
+    let lose = false;
+    const flower = [];
+    let score = 0;
+    while(bombs.length<16){
+        const die = Random(1, 100);
+        if(bombs.includes(die) === false){
+            bombs.push(die);
+        }
+    }
+    console.log(bombs)
+    while(!lose && flower.length<difficulty){
+        let Num;
+        do{Num=Number(prompt("Prego inserire numero"));}
+        while (isNaN(Num)||Num<1||Num>100);
+        if(bombs.includes(Num)){
+            lose=true;
+        }else if(flower.includes(Num)){
+            alert("Scegli un altro numero")
+        }else{flower.push(Num)
+            score = score +1;
+        }
+
+    }
+    if(lose){
+        alert(`Sei Morto con :${score} Punti`)
+    } else{
+        alert(`Sei Sopravvisuto per questa volta`)
     }
 
-    for(let i = 0; i <= 15 ; i++){
-    Box1.push(Random());
-    }
